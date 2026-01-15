816c4532be6429288b69aea830fc4a3e017e87b1
Michał Chęciński <michal.checinski@outlook.com>
2026-01-15 23:36:44 +0100

    Add GoProUUID object with predefined UUIDs for GoPro services

---
+++ app/src/main/kotlin/com/karoocameracontrol/extension/GoProUUID.kt
@@ -0,0 +1,11 @@
+package com.karoocameracontrol.extension
+
+import java.util.UUID
+
+object GoProUUID {
+    val GOPRO_SERVICE = UUID.fromString("0000FEA6-0000-1000-8000-00805F9B34FB")
+    val WIFI_AP_PASSWORD = UUID.fromString("b5f90003-aa8d-463e-86eb-03bd018a32a8")
+    val COMMAND = UUID.fromString("b5f90072-aa8d-463e-86eb-03bd018a32a8")
+    val SETTING = UUID.fromString("b5f90074-aa8d-463e-86eb-03bd018a32a8")
+    val QUERY = UUID.fromString("b5f90076-aa8d-463e-86eb-03bd018a32a8")
+}

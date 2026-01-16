3fa85d076521c3c73a2d29037974ce51708ff4ef
Michał Chęciński <michal.checinski@outlook.com>
2026-01-17 00:16:56 +0100

    Fix environment variable names for GitHub credentials in CI workflow and settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,8 +14,8 @@ dependencyResolutionManagement {
         maven {
             url = uri("https://maven.pkg.github.com/hammerheadnav/karoo-ext")
             credentials {
-                username = providers.gradleProperty("gpr.user").getOrElse(System.getenv("USERNAME"))
-                password = providers.gradleProperty("gpr.key").getOrElse(System.getenv("TOKEN"))
+                username = providers.gradleProperty("gpr.user").getOrElse(System.getenv("GH_USER"))
+                password = providers.gradleProperty("gpr.key").getOrElse(System.getenv("GH_TOKEN"))
             }
         }
     }

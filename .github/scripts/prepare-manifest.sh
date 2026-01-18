6108ca68021343dfc8d8488591702c25efdf76c6
Michał Chęciński <michal.checinski@outlook.com>
2026-01-18 23:40:53 +0100

    Refactor manifest preparation script to avoid in-place editing with sed

--- .github/scripts/prepare-manifest.sh
+++ .github/scripts/prepare-manifest.sh
@@ -11,13 +11,13 @@ cp manifest-template.json manifest.json
 
 # replace __VERSION_NUMBER_PLACEHOLDER__ with the actual version from input
 VERSION=${1}
-sed -i '' "s/__VERSION_NUMBER_PLACEHOLDER__/${VERSION}/g" manifest.json
+sed "s/__VERSION_NUMBER_PLACEHOLDER__/${VERSION}/g" manifest.json > manifest.json.tmp && mv manifest.json.tmp manifest.json
 
 # __VERSION_CODE_PLACEHOLDER__
-sed -i '' "s/__VERSION_CODE_PLACEHOLDER__/${2}/g" manifest.json
+sed "s/__VERSION_CODE_PLACEHOLDER__/${2}/g" manifest.json > manifest.json.tmp && mv manifest.json.tmp manifest.json
 
 # __RELEASE_NOTES_PLACEHOLDER__
 RELEASE_NOTES=$(cat "${3}" | sed 's/$/\\n/' | tr -d '\n' | sed 's/$/\\n/')
-sed -i '' "s|__RELEASE_NOTES_PLACEHOLDER__|${RELEASE_NOTES%\\n}|g" manifest.json
+sed "s|__RELEASE_NOTES_PLACEHOLDER__|${RELEASE_NOTES%\\n}|g" manifest.json > manifest.json.tmp && mv manifest.json.tmp manifest.json
 
 echo "Manifest prepared."

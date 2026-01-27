eaddc6df969377d5d28822af84fea3851ad8ad2d
Michał Chęciński <michal@checinski.me>
2026-01-27 19:13:59 +0100

    Fix release assets (#28)
    * Add APK SHA256 placeholder to manifest and update URLs in template
    
    * Add SHA256 checksum generation for APK in release workflow
    
    * Add pull request template to changelog configuration
    
    * Enhance release workflow to fetch latest version based on beta flag and update changelog generation
    
    * Add condition to approve release step for main branch only

--- .github/scripts/prepare-manifest.sh
+++ .github/scripts/prepare-manifest.sh
@@ -20,4 +20,7 @@ sed "s/__VERSION_CODE_PLACEHOLDER__/${2}/g" manifest.json > manifest.json.tmp &&
 RELEASE_NOTES=$(cat "${3}" | sed 's/$/\\n/' | tr -d '\n' | sed 's/$/\\n/')
 sed "s|__RELEASE_NOTES_PLACEHOLDER__|${RELEASE_NOTES%\\n}|g" manifest.json > manifest.json.tmp && mv manifest.json.tmp manifest.json
 
+APK_SHA256=${4}
+sed "s/__APK_SHA256_PLACEHOLDER__/${APK_SHA256}/g" manifest.json > manifest.json.tmp && mv manifest.json.tmp manifest.json
+
 echo "Manifest prepared."
